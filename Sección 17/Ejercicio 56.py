255f7ccfe041f54f26217e0edeafe0012b7ed440
GorkyAnge <92492732+GorkyAnge@users.noreply.github.com>
2023-06-04 14:21:24 -0500

    Create Ejercicio 56.py

---
+++ Sección 17/Ejercicio 56.py
@@ -0,0 +1,17 @@
+def two_sum(nums, target):
+    num_dict = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_dict:
+            return [num_dict[complement], i]
+        num_dict[num] = i
+    return []
+
+print(two_sum([2, 7, 11, 15], 9))
+print(two_sum([3, 2, 4], 6))
+print(two_sum([3, 3], 6))
+print(two_sum([2, 1, 2, 7, 11, 15], 9))
+print(two_sum([1, 2, 3, 4, 5], 10))
+print(two_sum([1, 2, 3, 4, 5], 7))
+print(two_sum([1, 2, 3, 4, 5], 3))
+print(two_sum([], 0))
