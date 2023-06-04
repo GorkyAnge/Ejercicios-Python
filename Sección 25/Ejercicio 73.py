96cc7ed802118c9f5f761a71918fb6c6722cbd2d
Pollito Tactico <114546336+PollitoTactico@users.noreply.github.com>
2023-06-04 14:49:01 -0500

    Create Ejercicio 73.py

---
+++ Sección 25/Ejercicio 73.py
@@ -0,0 +1,84 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def bubble_sort(self):
+        if self.length < 2:
+            return
+
+        sorted_list = False
+        while not sorted_list:
+            sorted_list = True
+            current = self.head
+            while current.next is not None:
+                if current.value > current.next.value:
+                    self.__swap_nodes(current, current.next)
+                    sorted_list = False
+                current = current.next
+
+    def __swap_nodes(self, node1, node2):
+        temp = node1.value
+        node1.value = node2.value
+        node2.value = temp
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(2)
+my_linked_list.append(6)
+my_linked_list.append(5)
+my_linked_list.append(1)
+my_linked_list.append(3)
+
+print("Linked List Before Sort:")
+my_linked_list.print_list()
+
+my_linked_list.bubble_sort()
+
+print("\nSorted Linked List:")
+my_linked_list.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Linked List Before Sort:
+    4
+    2
+    6
+    5
+    1
+    3
+
+    Sorted Linked List:
+    1
+    2
+    3
+    4
+    5
+    6
+
+"""
+
