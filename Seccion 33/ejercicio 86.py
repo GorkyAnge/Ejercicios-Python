708c8f2d14af243180dd5d978b4adc19e04177be
cami sanchez <102784592+camilasanchezu@users.noreply.github.com>
2023-06-04 14:31:20 -0500

    Create ejercicio 86.py

---
+++ Seccion 33/ejercicio 86.py
@@ -0,0 +1,74 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else: 
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def kth_smallest(self, k):
+        count=[0]
+        result=[None]
+        
+        def inorder(node):
+            if node is None:
+                return
+        
+            inorder(node.left)
+            count[0]+= 1
+        
+            if count[0]==k:
+                result[0]=node.value
+                return
+            inorder(node.right)
+        
+        inorder(self.root)
+        return result[0]
+
+
+bst = BinarySearchTree()
+
+bst.insert(5)
+bst.insert(3)
+bst.insert(7)
+bst.insert(2)
+bst.insert(4)
+bst.insert(6)
+bst.insert(8)
+
+print(bst.kth_smallest(1))  # Expected output: 2
+print(bst.kth_smallest(3))  # Expected output: 4
+print(bst.kth_smallest(6))  # Expected output: 7
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    2
+    4
+    7
+
+ """
