380b163276d92ee5a796e59e7eac6e54c21a4eae
cami sanchez <102784592+camilasanchezu@users.noreply.github.com>
2023-06-04 13:58:05 -0500

    Create ejercicio 93.py

---
+++ Seccion34/ejercicio 93.py
@@ -0,0 +1,23 @@
+def max_subarray(nums):
+    if not nums:
+        return 0
+
+    current_sum = max_sum = nums[0]
+
+    for i in range(1, len(nums)):
+        current_sum = max(nums[i], current_sum + nums[i])
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+input_case_1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+result_1 = max_subarray(input_case_1)
+print("Example 1: Input:", input_case_1, "\nResult:", result_1)  
+
+input_case_2 = [1, 2, 3, -4, 5, 6]
+result_2 = max_subarray(input_case_2)
+print("Example 2: Input:", input_case_2, "\nResult:", result_2) 
+
+input_case_3 = [-1, -2, -3, -4, -5]
+result_3 = max_subarray(input_case_3)
+print("Example 3: Input:", input_case_3, "\nResult:", result_3)
